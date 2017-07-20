1fb55edc4b4c3fbbc5e0b19a6c553bc842cad3dd
Dennis B <bluz71@users.noreply.github.com>
2017-07-20 18:08:28 +1000

    Starting post show action.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
 export const CREATE_POST = "create_posts";
+export const FETCH_POST  = "fetch_posts";
 const ROOT_URL = "https://reduxblog.herokuapp.com/api";
 const API_KEY  = "?key=JANECAT1234";
 
@@ -16,10 +17,19 @@ export function fetchPosts() {
 
 export function createPost(values, callback) {
   const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
-    .then(() => { callback() });
+    .then(() => { callback(); });
 
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,14 @@
-import { FETCH_POSTS } from "../actions/index";
 import _ from "lodash";
+import { FETCH_POSTS, FETCH_POST } from "../actions/index";
 
 // State will be an object, hence make the default state argument an empty
 // object.
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST: {
+      const post = action.payload.data;
+      return { ...state, [post.id]: post };
+    }
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, "id");
     default:
